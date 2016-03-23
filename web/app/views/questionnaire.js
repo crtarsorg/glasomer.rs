cd0c0d5297fdd97532306ec8d000f82b7a46b51c
Vullkan92 <vullkan.halili@riinvest.net>
2016-03-23 16:12:32 +0100

    added if helper for handlebars template

--- web/app/views/questionnaire.js
+++ web/app/views/questionnaire.js
@@ -10,6 +10,12 @@ window.QuestionnaireView = Backbone.View.extend({
             this.name = value;
         });
 
+        Handlebars.registerHelper('if_eq', function (a, b, opts) {
+            if (a == b) // Or === depending on your needs
+                return opts.fn(this);
+            else
+                return opts.inverse(this);
+        });
         return this
     },
     events: {
